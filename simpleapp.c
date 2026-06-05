4b00cad2de7afed05ebf0b487a421232be7b1dcc
Tyler Fullerton <tfullert@gmail.com>
2026-06-04 20:17:51 -0700

    Update simpleapp.c

--- simpleapp.c
+++ simpleapp.c
@@ -5,6 +5,8 @@
 // I needed to change RUNNER_TEMP_DIR to RUNNER_TEMP and I also needed to run a PowerShell command to base64 encode the .p12 file:
 // [Convert]::ToBase64String([IO.File]::ReadAllBytes("C:\path_to_file\Certificate_pkcs12.p12")) | Set-Content -NoNewline -Encoding ascii "C:\path_to_file\Certificate_pkcs12.p12.b64"
 
+// Getting an issue with the Client Auth Cert not working.
+
 int main() {
     // Say "Hello" to the world!
     printf("Hello, World!\n");
