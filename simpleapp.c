8e9427f80da95266c6f9879ac9f45fd76d015b05
Tyler Fullerton <tfullert@gmail.com>
2026-06-04 19:33:07 -0700

    Update simpleapp.c

--- simpleapp.c
+++ simpleapp.c
@@ -2,6 +2,8 @@
 
 // This code will be updated to trigger Actions
 // This should be working.
+// I needed to change RUNNER_TEMP_DIR to RUNNER_TEMP and I also needed to run a PowerShell command to base64 encode the .p12 file:
+// [Convert]::ToBase64String([IO.File]::ReadAllBytes("C:\path_to_file\Certificate_pkcs12.p12")) | Set-Content -NoNewline -Encoding ascii "C:\path_to_file\Certificate_pkcs12.p12.b64"
 
 int main() {
     // Say "Hello" to the world!
