092b7a87cbf8e3f23682e157f2e8694a3d96cb69
Tyler Fullerton <tfullert@gmail.com>
2026-06-13 10:21:17 -0700

    Update simpleapp.c

--- simpleapp.c
+++ simpleapp.c
@@ -2,7 +2,6 @@
 #include <zlib.h>
 
 // This code will be updated to trigger Actions
-// This should be working.
 // I needed to change RUNNER_TEMP_DIR to RUNNER_TEMP and I also needed to run a PowerShell command to base64 encode the .p12 file:
 // [Convert]::ToBase64String([IO.File]::ReadAllBytes("C:\path_to_file\Certificate_pkcs12.p12")) | Set-Content -NoNewline -Encoding ascii "C:\path_to_file\Certificate_pkcs12.p12.b64"
 // Make sure YAML file references SM_CLIENT_CERT_FILE properly (using env.SMCLIENT_CERT_FILE): SM_CLIENT_CERT_FILE: ${env.SM_CLIENT_CERT_FILE}
